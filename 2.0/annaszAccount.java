dd2338ff322f3d09680537e9bbd777c728ef95ca
Marcin Pasynczuk <110817419+MarcinPasynczuk@users.noreply.github.com>
2022-11-20 02:47:29 +0100

    Update annaszAccount.java

---
+++ 2.0/annaszAccount.java
@@ -0,0 +1,20 @@
+public class annaszAccount { private static int accountNumber = 487300;private static double balance = 0.00;private static String customerName = "ania";private static String email = "ania@#wp.pl";private static String password = "kucis123";public static int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public static double getBalance() {
+        return balance;
+    }
+
+    public static String getCustomerName() {
+        return customerName;
+    }
+
+    public static String getEmail() {
+        return email;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+}
