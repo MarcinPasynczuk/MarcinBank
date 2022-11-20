d929a6632c217f714bf4d0e558c6aed4ba223d27
Marcin Pasynczuk <110817419+MarcinPasynczuk@users.noreply.github.com>
2022-11-20 02:48:19 +0100

    Update marcinAccount.java

---
+++ 2.0/marcinAccount.java
@@ -0,0 +1,28 @@
+public class marcinAccount {
+    private static int accountNumber = 12345;
+    private static double balance = 100.00;
+    private static String customerName = "Marcin";
+    private static String email = "marcin@gmail.com";
+    private static String password = "password123";
+
+
+    public static int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public static double getBalance() {
+        return balance;
+    }
+
+    public static String getCustomerName() {
+        return customerName;
+    }
+
+    public static String getEmail() {
+        return email;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+}
