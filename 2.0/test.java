a72cab0417edd3f6be22185f896563ddcf089bb3
Marcin Pasynczuk <110817419+MarcinPasynczuk@users.noreply.github.com>
2022-11-20 02:48:35 +0100

    Update test.java

---
+++ 2.0/test.java
@@ -0,0 +1,100 @@
+import java.io.File;  // Import the File class
+import java.io.FileWriter;
+import java.io.IOException;  // Import the IOException class to handle errors
+
+//public class test {
+//    public static void main(String[] args) {
+//        String text = "123";
+////        String text = "src/" + accountNumber;
+//        try {
+//
+//            File myObj = new File(text +"Account.java");
+//            if (myObj.createNewFile()) {
+//                System.out.println("File created: " + myObj.getName());
+//            } else {
+//                System.out.println("File already exists.");
+//            }
+//        } catch (IOException e) {
+//            System.out.println("An error occurred.");
+//            e.printStackTrace();
+//        }
+//
+//        String program = "public class " + input +
+//                "marcinAccount {\n" +
+//                "    private static String accountNumber = \"number\";\n" +
+//                "    private static double balance = 0.00;\n" +
+//                "    private static String customerName = \"name\";\n" +
+//                "    private static String email = \"name@gmail.com\";\n" +
+//                "    private static String password = \"password\";\n" +
+//                "\n" +
+//                "\n" +
+//                "    public static String getAccountNumber() {\n" +
+//                "        return accountNumber;\n" +
+//                "    }\n" +
+//                "\n" +
+//                "    public static double getBalance() {\n" +
+//                "        return balance;\n" +
+//                "    }\n" +
+//                "\n" +
+//                "    public static String getCustomerName() {\n" +
+//                "        return customerName;\n" +
+//                "    }\n" +
+//                "\n" +
+//                "    public static String getEmail() {\n" +
+//                "        return email;\n" +
+//                "    }\n" +
+//                "\n" +
+//                "    public static String getPassword() {\n" +
+//                "        return password;\n" +
+//                "    }\n" +
+//                "}";
+//        try {
+//            // Creates a Writer using FileWriter
+//            FileWriter output = new FileWriter(text +"Account.java");
+//
+//            // Writes the program to file
+//            output.write(program);
+//            System.out.println("Data is written to the file.");
+//
+//            // Closes the writer
+//            output.close();
+//        }
+//        catch (Exception e) {
+//            e.getStackTrace();
+//        }
+//
+//
+//
+//    }
+//}
+
+
+// NIE WIEM CO ROBIE
+
+//        try {
+//            File f = new File("src");
+//
+//            FilenameFilter filter = new FilenameFilter() {
+//                @Override
+//                public boolean accept(File f, String name) {
+//                    // We want to find only .c files
+//                    return name.endsWith(accountNumber + "Account.java");
+//                }
+//            };
+//
+//            // Note that this time we are using a File class as an array,
+//            // instead of String
+//            File[] files = f.listFiles(filter);
+//
+//            // Get the names of the files by using the .getName() method
+//            for (int i = 0; i < files.length; i++) {
+//                System.out.println(files[i].getName());
+//            }
+//        } catch (Exception e) {
+//            System.err.println(e.getMessage());
+//        }
+//
+//        System.out.println();
+
+
+// NIE WIEM CO ROBIE
